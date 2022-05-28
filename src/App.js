952bfd9a67f2c91464f8d6305c8fd94dc1d16d36
Everson Alves de Rezende <rezende.everson@gmail.com>
2022-05-28 13:15:19 -0300

    Alterando o App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Seja bem vindo Everson Rezende</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
+
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -23,3 +40,5 @@ function App() {
 }
 
 export default App;
+
+*/
