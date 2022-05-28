2016a23129e6d892a9aadb923bb9d1f3cc874ecc
Everson Alves de Rezende <rezende.everson@gmail.com>
2022-05-28 16:07:09 -0300

    fix the error of AmmplifySignOut

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
-import {
-  withAuthenticator,
-  AmplifySignOut,
-  Authenticator
-} from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
+//import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react-v1' //usar esse import para corrigir o AmplifySignOut
 
 function App() {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <div className="App">
-          <header>
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1>We now have Auth!</h1>
-          </header>
-        </div>
-      )}
-    </Authenticator>
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
+    </div>
   )
 }
 
