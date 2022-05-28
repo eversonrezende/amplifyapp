515aac19beaa833cb8c59d97728b6e2a2a220af9
Everson Alves de Rezende <rezende.everson@gmail.com>
2022-05-28 14:17:12 -0300

    includes Authenticator service

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import {
+  withAuthenticator,
+  AmplifySignOut,
+  Authenticator
+} from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Seja bem vindo Everson Rezende</h1>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <header>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>We now have Auth!</h1>
+          </header>
+        </div>
+      )}
+    </Authenticator>
   )
 }
 
-export default App
+export default withAuthenticator(App)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+import { I18n } from 'aws-amplify'
+import AmplifyI18n from 'amplify-i18n'
+Amplify.configure(config)
+const locales = ['en', 'fr', 'pt-BR']
+AmplifyI18n.configure(locales)
+I18n.setLanguage('pt-BR')
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
